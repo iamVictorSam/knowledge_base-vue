839b297e16ece7bfe1c4d566e5d08620b1cc53e5
Iamvictorsam <iamvictorsam@gmail.com>
2023-01-18 12:55:08 +0100

    all done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,77 +1,15 @@
 <script>
 import Butter from "buttercms";
 
-class Post {
-  constructor(title, link, author, img) {
-    this.title = title;
-    this.link = link;
-    this.author = author;
-    this.img = img;
-  }
-}
+
 
 export default {
   data() {
     return {
       search: "",
-      fruits: [],
+
       body: [],
-      postList: [
-        new Post(
-          "Vue.js",
-          "https://vuejs.org/",
-          "Chris",
-          "https://vuejs.org//images/logo.png"
-        ),
-        new Post(
-          "React.js",
-          "https://facebook.github.io/react/",
-          "Tim",
-          "https://daynin.github.io/clojurescript-presentation/img/react-logo.png"
-        ),
-        new Post(
-          "Angular.js",
-          "https://angularjs.org/",
-          "Sam",
-          "https://angularjs.org/img/ng-logo.png"
-        ),
-        new Post(
-          "Ember.js",
-          "http://emberjs.com/",
-          "Rachel",
-          "http://www.gravatar.com/avatar/0cf15665a9146ba852bf042b0652780a?s=200"
-        ),
-        new Post(
-          "Meteor.js",
-          "https://www.meteor.com/",
-          "Chris",
-          "http://hacktivist.in/introduction-to-nodejs-mongodb-meteor/img/meteor.png"
-        ),
-        new Post(
-          "Aurelia",
-          "http://aurelia.io/",
-          "Tim",
-          "https://cdn.auth0.com/blog/aurelia-logo.png"
-        ),
-        new Post(
-          "Node.js",
-          "https://nodejs.org/en/",
-          "A. A. Ron",
-          "https://code-maven.com/img/node.png"
-        ),
-        new Post(
-          "Pusher",
-          "https://pusher.com/",
-          "Alex",
-          "https://avatars1.githubusercontent.com/u/739550?v=3&s=400"
-        ),
-        new Post(
-          "Feathers.js",
-          "http://feathersjs.com/",
-          "Chuck",
-          "https://cdn.worldvectorlogo.com/logos/feathersjs.svg"
-        ),
-      ],
+      
     };
   },
 
@@ -84,8 +22,6 @@ export default {
       .then((response) => {
         console.log(response);
         ({ body: this.body } = response.data.data.fields);
-
-        console.log(this.body, "this", typeof this.body);
       })
       .catch(function (resp) {
         console.log(resp);
@@ -98,76 +34,23 @@ export default {
         return post.question.toLowerCase().includes(this.search.toLowerCase());
       });
     },
-
-    // filteredList() {
-    //   // const fruits = ["apple", "banana", "orange"];
-    //   return this.fruits.filter((fruit) => {
-    //     return fruit.toLowerCase().includes(this.input.toLowerCase());
-    //   });
-    // },
-    // filteredPeople() {
-    //   if (this.people) {
-    //     return this.people.filter((person) => {
-    //        return person.name.match(this.search);
-    //     });
-    //   }
-    //   return false;
-    // }
   },
 
-  // import { ref, onMounted } from "vue";
-  // import { $ref } from "vue/macros";
-
-  // let input = ref("");
-  // let arr = ref([]);
-
-  // onMounted(() => {
-  //   const butter = Butter(import.meta.env.VITE_BUTTER_API_KEY);
-
-  //   butter.page
-  //     .retrieve("*", "how-to")
-  //     .then((response) => {
-  //       console.log(response);
-  //       // ({ body } = response.data.data);
-  //       arr.value = response.data.data.fields.body;
-  //       console.log(arr.value, typeof(arr.value));
-  //     })
-  //     .catch(function (resp) {
-  //       console.log(resp);
-  //     });
-  // });
-
-  //   // butter.page
-  //   //   .retrieve("*", "home")
-  //   //   .then((resp) => {
-  //   //     ({
-  //   //       image: this.image,
-  //   //       title: this.title,
-  //   //       description: this.description,
-  //   //     } = resp.data.data.fields.header_component);
-  //   //   })
-  // .catch(function (resp) {
-  //   console.log(resp);
-  // });
-  // butter.page
-  //   .retrieve("*", "how-to")
-  //   .then((response) => console.log(response));
 };
 </script>
 
 <template>
   <div id="app">
     <div class="search-wrapper">
-      <input type="text" v-model="search" placeholder="Search title.." />
-      <label>Search title:</label>
+      <input type="text" v-model="search" placeholder="Search here.." />
+    <label>Search Question</label>
     </div>
     <div class="wrapper">
       <div class="card" v-for="post in filteredList">
         <div class="timeDiv">
-          <span class="green">{{ post.answered_by }}</span>
-          <span>{{ new Date("2022-12-26T14:30:00").getUTCDate() }}</span>
+          <div class="green">{{ post.answered_by }}</div>
+          <span>{{ post.answered_on.slice(0, 10)}}</span> 
         </div>
-
         <h2>{{ post.question }}</h2>
         <p v-html="post.answer"></p>
       </div>
@@ -196,29 +79,33 @@ div#app {
     position: relative;
     label {
       position: absolute;
-      font-size: 12px;
+      font-size: 18px;
       color: rgba(0, 0, 0, 0.5);
       top: 8px;
-      left: 12px;
+      left: 2px;
       z-index: -1;
       transition: 0.15s all ease-in-out;
     }
     input {
-      padding: 4px 12px;
+      padding: 20px 12px;
+      margin-bottom: 30px;
+      width: 600px;
       color: rgba(0, 0, 0, 0.7);
       border: 1px solid rgba(0, 0, 0, 0.12);
+      border-radius: 20px;
       transition: 0.15s all ease-in-out;
       background: white;
+      font-size: 18px;
       &:focus {
         outline: none;
-        transform: scale(1.05);
+        transform: scale(1.5);
         & + label {
-          font-size: 10px;
-          transform: translateY(-24px) translateX(-12px);
+          font-size: 18px;
+          transform: translateY(-64px) translateX(-12px);
         }
       }
       &::-webkit-input-placeholder {
-        font-size: 12px;
+        font-size: 18px;
         color: rgba(0, 0, 0, 0.5);
         font-weight: 100;
       }
@@ -228,17 +115,20 @@ div#app {
   .wrapper {
     display: flex;
     /* max-width: 854px; */
+    flex-direction: column;
     flex-wrap: wrap;
     padding-top: 12px;
     text-align: start;
   }
 
   .card {
-    // background-color: white;
-    box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px,
+   background-color: black;
+   border-radius: 10px;
+   padding: 60px;
+    box-shadow: rgba(0, 0, 0, 0.117647) 0px 4px 10px,
       rgba(0, 0, 0, 0.117647) 0px 1px 4px;
-    /* max-width: 124px; */
-    margin: 12px;
+    width:800px;
+    margin: 20px;
     transition: 0.15s all ease-in-out;
     &:hover {
       transform: scale(1.1);
@@ -271,9 +161,11 @@ div#app {
   }
 
   .green {
-    padding: 0 5px;
+    padding: 0 7px;
+    text-align: center;
     border-radius: 10px;
     background: green;
+    font-weight: bold;
   }
 
   .box {
